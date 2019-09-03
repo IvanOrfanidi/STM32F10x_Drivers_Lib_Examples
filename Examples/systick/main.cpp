7f30cce89872abd24d43d73ea49cb90225c7604d
IvanOrfanidi <i.orfanidi@thirdpin.io>
2019-09-03 14:13:19 +0300

    Added an example of using the SysTick timer.

--- Examples/systick/main.cpp
+++ Examples/systick/main.cpp
@@ -11,8 +11,18 @@
  */
 int main()
 {
+    // Get Instance
     auto systick = Systick::getInstance();
 
+    // Configure 1 tick - 1 msec
+    systick->init(SystemCoreClock, 1000);
+
+    auto count = 0U;
     while(true) {
+        // Delay 1 Sec
+        systick->delay(1000);
+
+        // Out info
+        std::cout << ++count << std::endl;
     }
 }
